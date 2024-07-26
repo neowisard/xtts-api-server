2a857063925ad6994576efa3420ef6e982089df5
Neow15ard <n@uwns.org>
2024-07-26 21:52:36 +0300

    add normalize from silero

--- xtts_api_server/tts_funcs.py
+++ xtts_api_server/tts_funcs.py
@@ -146,10 +146,10 @@ class DateTimeNormalizer:
 
     def normalize(self, text: str) -> str:
         text = " ".join(text.split())
-        text = self.normalize_number(text)
+        #text = self.normalize_number(text)
         #text = self.translit_text(text)
-        #text = self.normalize_date(text)
-        #text = self.normalize_time(text)
+        text = self.normalize_date(text)
+        text = self.normalize_time(text)
         return text
 
 
